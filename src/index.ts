3616b2cd10738297f2f7ad38b09c4dc7f9be51c3
gurelbs <guri240@gmail.com>
2021-09-23 20:54:48 +0300

    'updated'

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ export class Answers {
   async news(term: string | string[], lang: string = 'he') {
     try {
       const answer = await getNews(term, lang);
-      // eslint-disable-next-line no-console
-      console.info(answer);
+      // // eslint-disable-next-line no-console
+      // console.info(answer);
       return answer
     } catch (error) {
       return error;
