f92f330d892e114169064d34d77e69ba58b5558a
gurelbs <guri240@gmail.com>
2021-09-23 20:46:56 +0300

    'updated'

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export class Answers {
   async news(term: string | string[], lang: string = 'he') {
     try {
       const answer = await getNews(term, lang);
-      // console.info(answer);
+      console.info(answer);
       return answer
     } catch (error) {
       return error;
