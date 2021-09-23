2452b33943bec75f0b0dd73c8dd538e8cbbca72d
gurelbs <guri240@gmail.com>
2021-09-23 19:14:45 +0300

    'updated'

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import {getNews} from './news/getNews';
-
-export default class Answers {
+import {getNews}  from './news/getNews';
+export {getNews};
+export class Answers {
   async news(term: string | string[], lang: string = 'he') {
     try {
       return await getNews(term, lang);
