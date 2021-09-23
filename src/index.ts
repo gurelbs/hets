b61da15feb406f0ae67c5ad52d9204d9e34bf897
gurelbs <guri240@gmail.com>
2021-09-23 20:33:52 +0300

    'updated'

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import getNews from './news/getNews';
 export default class Answers {
   async news(term: string | string[], lang: string = 'he') {
     try {
-      let answer = await getNews(term, lang);
-      console.info(answer);
+      const answer = await getNews(term, lang);
+      // console.info(answer);
       return answer
     } catch (error) {
       return error;
