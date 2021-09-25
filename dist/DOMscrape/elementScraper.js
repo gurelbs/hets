af65a226da3abc1a4810f2ba8b4eda42f0b55241
gurelbs <guri240@gmail.com>
2021-09-26 01:02:07 +0300

    'updated'

--- dist/DOMscrape/elementScraper.js
+++ dist/DOMscrape/elementScraper.js
@@ -22,8 +22,50 @@ function elementScraper(url, selector) {
         yield page.goto(url);
         yield page.waitForSelector(selector);
         const currentElement = yield page.$(selector);
-        const currentElementCount = yield page.evaluate(el => el === null || el === void 0 ? void 0 : el.childElementCount, currentElement);
-        result.push({ counter: currentElementCount });
+        yield page.evaluate((el, i) => {
+            if (el) {
+                result.push({
+                    number: `${++i}`,
+                    name: el.name,
+                    childNumber: el.childElementCount,
+                    innerText: el.innerText,
+                    innerHTML: el.innerHTML,
+                    attributes: el.attributes,
+                    className: el.className,
+                    id: el.id,
+                    value: el.value,
+                    checked: el.checked,
+                    selected: el.selected,
+                    disabled: el.disabled,
+                    type: el.type,
+                    placeholder: el.placeholder,
+                    src: el.src,
+                    href: el.href
+                });
+                if (el.childElementCount > 0) {
+                    el.childNodes.forEach((child) => {
+                        result.push({
+                            number: `${++i}`,
+                            name: child.name,
+                            childNumber: child.childElementCount,
+                            innerText: child.innerText,
+                            innerHTML: child.innerHTML,
+                            attributes: child.attributes,
+                            className: child.className,
+                            id: child.id,
+                            value: child.value,
+                            checked: child.checked,
+                            selected: child.selected,
+                            disabled: child.disabled,
+                            type: child.type,
+                            placeholder: child.placeholder,
+                            src: child.src,
+                            href: child.href
+                        });
+                    });
+                }
+            }
+        }, currentElement);
         yield browser.close();
         return result;
     });
--- src/DOMscrape/elementScraper.ts
+++ src/DOMscrape/elementScraper.ts
@@ -7,8 +7,50 @@ export async function elementScraper(url:string, selector:string): Promise<strin
   await page.goto(url)
   await page.waitForSelector(selector)
   const currentElement = await page.$(selector)
-  const currentElementCount = await page.evaluate(el => el?.childElementCount,currentElement)
-  result.push({counter: currentElementCount})
+  await page.evaluate((el, i) => {
+    if (el) {
+      result.push({
+        number: `${++i}`,
+        name: el.name,
+        childNumber: el.childElementCount,
+        innerText: el.innerText,
+        innerHTML: el.innerHTML,
+        attributes: el.attributes,
+        className: el.className,
+        id: el.id,
+        value: el.value,
+        checked: el.checked,
+        selected: el.selected,
+        disabled: el.disabled,
+        type: el.type,
+        placeholder: el.placeholder,
+        src: el.src,
+        href: el.href  
+      })
+      if (el.childElementCount > 0) {
+        el.childNodes.forEach((child:any) => {
+          result.push({
+            number: `${++i}`,
+            name: child.name,
+            childNumber: child.childElementCount,
+            innerText: child.innerText,
+            innerHTML: child.innerHTML,
+            attributes: child.attributes,
+            className: child.className,
+            id: child.id,
+            value: child.value,
+            checked: child.checked,
+            selected: child.selected,
+            disabled: child.disabled,
+            type: child.type,
+            placeholder: child.placeholder,
+            src: child.src,
+            href: child.href
+          })
+        })
+      }
+    }
+  },currentElement)
   await browser.close()
   return result
 }
