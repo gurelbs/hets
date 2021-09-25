f1ab060d64ea1592b69c43ea6e75f6fbff3b6db7
gurelbs <guri240@gmail.com>
2021-09-26 01:22:57 +0300

    'updated'

--- dist/DOMscrape/elementScraper.js
+++ dist/DOMscrape/elementScraper.js
@@ -16,14 +16,14 @@ exports.elementScraper = void 0;
 const puppeteer_1 = __importDefault(require("puppeteer"));
 function elementScraper(url, selector) {
     return __awaiter(this, void 0, void 0, function* () {
-        const browser = yield puppeteer_1.default.launch({ slowMo: 300, headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+        const browser = yield puppeteer_1.default.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
         const page = yield browser.newPage();
         yield page.goto(url);
         yield page.waitForSelector(selector);
         const currentElement = yield page.$(selector);
         const res = yield page.evaluate((el, i) => {
             const result = [];
-            if (el) {
+            if (el && el.childElementCount > 0) {
                 result.push({
                     number: `${++i}`,
                     name: el.name,
@@ -42,28 +42,27 @@ function elementScraper(url, selector) {
                     src: el.src,
                     href: el.href,
                 });
-                if (el.childElementCount > 0) {
-                    [...el.childNodes].map((child) => {
-                        result.push({
-                            number: `${++i}`,
-                            name: child.name,
-                            childNumber: child.childElementCount,
-                            innerText: child.innerText,
-                            innerHTML: child.innerHTML,
-                            attributes: child.attributes,
-                            className: child.className,
-                            id: child.id,
-                            value: child.value,
-                            checked: child.checked,
-                            selected: child.selected,
-                            disabled: child.disabled,
-                            type: child.type,
-                            placeholder: child.placeholder,
-                            src: child.src,
-                            href: child.href
-                        });
-                    });
-                }
+                el.children.forEach((child) => elementScraper(url, child));
+            }
+            else {
+                result.push({
+                    number: `${++i}`,
+                    name: el.name,
+                    childNumber: el.childElementCount,
+                    innerText: el.innerText,
+                    innerHTML: el.innerHTML,
+                    attributes: el.attributes,
+                    className: el.className,
+                    id: el.id,
+                    value: el.value,
+                    checked: el.checked,
+                    selected: el.selected,
+                    disabled: el.disabled,
+                    type: el.type,
+                    placeholder: el.placeholder,
+                    src: el.src,
+                    href: el.href,
+                });
             }
             return result;
         }, currentElement);
--- src/DOMscrape/elementScraper.ts
+++ src/DOMscrape/elementScraper.ts
@@ -1,14 +1,14 @@
 import puppeteer from 'puppeteer'
 
 export async function elementScraper(url:string, selector:string): Promise<any>{
-  const browser = await puppeteer.launch({ slowMo: 300, headless:false, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+  const browser = await puppeteer.launch({ headless:false, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
   const page = await browser.newPage()
   await page.goto(url)
   await page.waitForSelector(selector)
   const currentElement = await page.$(selector)
   const res = await page.evaluate((el, i) => {
     const result:any = [];
-    if (el) {
+    if (el && el.childElementCount > 0) {
       result.push({
         number: `${++i}`,
         name: el.name,
@@ -27,28 +27,26 @@ export async function elementScraper(url:string, selector:string): Promise<any>{
         src: el.src,
         href: el.href,
       })
-      if (el.childElementCount > 0) {
-        [...el.childNodes].map((child:any) => {
-          result.push({
-            number: `${++i}`,
-            name: child.name,
-            childNumber: child.childElementCount,
-            innerText: child.innerText,
-            innerHTML: child.innerHTML,
-            attributes: child.attributes,
-            className: child.className,
-            id: child.id,
-            value: child.value,
-            checked: child.checked,
-            selected: child.selected,
-            disabled: child.disabled,
-            type: child.type,
-            placeholder: child.placeholder,
-            src: child.src,
-            href: child.href
-          })
-        })
-      }
+      el.children.forEach((child:any) => elementScraper(url, child))
+    } else {
+      result.push({
+        number: `${++i}`,
+        name: el.name,
+        childNumber: el.childElementCount,
+        innerText: el.innerText,
+        innerHTML: el.innerHTML,
+        attributes: el.attributes,
+        className: el.className,
+        id: el.id,
+        value: el.value,
+        checked: el.checked,
+        selected: el.selected,
+        disabled: el.disabled,
+        type: el.type,
+        placeholder: el.placeholder,
+        src: el.src,
+        href: el.href,
+      })
     }
     return result
   },currentElement)
